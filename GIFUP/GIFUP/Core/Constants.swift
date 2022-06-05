1522e79d17bff33639684999f736920626ba5922
Onur Ustunel <ustunel.onur@gmail.com>
2022-06-05 17:47:34 +0300

    Folders are created

---
+++ GIFUP/GIFUP/Core/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  GIFUP
+//
+//  Created by onur on 5.06.2022.
+//
+
+import UIKit
+
+/// Application Constants
+struct Constants {
+    static let requestDuration = 10.0
+    static let firstLunchHeader = "Loading..."
+    static let searchCellHeader = "Search results:"
+    static let randomCellHeader = "Random selected GIF:"
+    static let searchBarPlaceholder = "Search"
+    static let sectionHeadHeight: CGFloat = 40.0
+}
